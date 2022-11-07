e6239600b4f884d5a97b86ee885e04593ddca057
yeeun153 <112635570+yeeun153@users.noreply.github.com>
2022-11-07 18:30:06 +0900

    Add files via upload

---
+++ Hello2.java
@@ -0,0 +1,20 @@
+package sec01;
+
+/**
+ * 콘솔에 '안녕' 메시지를 출력하는 첫 번째 자바 프로그램
+ */
+public class Hello2 {
+	public static void main(String[] args) {
+		/*
+		 * 메인 메소드 내부
+		 */
+		System.out.println("안녕!");	// 콘솔에 문자 입력
+		System.out.println("안녕" + "!!");
+		
+		String hello = "안녕!!!";
+		
+		System.out.println(hello);
+		
+	}
+}
+
