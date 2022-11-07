316f9a2e2b1b379ddc5dc651e18b2e72b9ac02e5
yeeun153 <112635570+yeeun153@users.noreply.github.com>
2022-11-07 18:28:03 +0900

    Add files via upload

--- TriangleTest.java
+++ TriangleTest.java
@@ -1,30 +1,45 @@
-package hw;
-
-public class TriangleTest{
-	public static void main(String[] args) {
-		Triangle t1 = new Triangle(10.0 , 5.0); 
-		System.out.println(t1.findArea());
-	}
-}
+package test;
 
 class Triangle{
 	private double base;
 	private double height;
 	
-	public Triangle(double base , double height) {
+	public Triangle(double base, double height) {
 		this.base = base;
 		this.height = height;
 	}
 	
+	public Triangle() {
+		this.base = 1.0;
+		this.height = 1.0;
+	}
+	
+	public Triangle(double b) {
+		base = b;
+		height = b;
+	}
+	
 	public double getBase() {
-		return base;
+		return this.base;
 	}
 	
 	public double getHeight() {
-		return height;
+		return this.height;
 	}
 	
-	public double findArea(){
-		return (base * height) / 2;
+	public double findArea() {
+		return (base*height)/2;
 	}
-} 
+}
+
+public class TriangleTest {
+
+	public static void main(String[] args) {
+		Triangle t1 = new Triangle(10.0 , 5.0);
+		Triangle t2 = new Triangle(); //밑변 1.0 , 높이 1.0
+		Triangle t3 = new Triangle(3.0); //밑변=높이=3.0
+		System.out.println(t1.findArea());
+		System.out.println("t2=>" + t2.findArea());
+		System.out.println("t3=>" + t3.findArea());
+	}
+}
