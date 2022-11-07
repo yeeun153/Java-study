ff9b8dd3cd9c2717d845c834af9418f096505fa4
yeeun153 <112635570+yeeun153@users.noreply.github.com>
2022-11-07 19:03:55 +0900

    Add files via upload

---
+++ Debug1Demo.java
@@ -0,0 +1,13 @@
+package sec04;
+
+public class Debug1Demo {
+	public static void main(String[] args) {
+		int d, m, n, n2, n3;
+		d = 0;
+		n = 2;
+		// n2 = n * n:
+		n2 = n * n;	
+		n3 = n2 * n2;
+		// m = n / d;
+	}
+}
