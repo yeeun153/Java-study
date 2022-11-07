063409e2e35bcb6c02ecb38b7c1cf923df19b602
yeeun153 <112635570+yeeun153@users.noreply.github.com>
2022-11-07 18:22:28 +0900

    Add files via upload

---
+++ CircleTest.java
@@ -0,0 +1,36 @@
+package hw;
+
+class Circle{
+	int radius;
+	
+	public Circle(int radius) {
+		this.radius = radius;
+	}
+	
+	public void show() {
+		System.out.println("반지름이 " + radius + "인 원이다.");
+	}
+}
+
+class ColoredCircle extends Circle{
+	String color;
+	
+	public ColoredCircle(int radius, String color){
+		super(radius);
+		this.color = color;
+	}
+
+	public void show() {
+		System.out.println("반지름이 " + radius + "인 " + color + " 원이다.");
+	}
+}
+
+public class CircleTest {
+
+	public static void main(String[] args) {
+		Circle c1 = new Circle(5);
+		ColoredCircle c2 = new ColoredCircle(10, "빨간색");
+		c1.show();
+		c2.show();
+	}
+}
