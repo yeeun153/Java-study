4deef76b92c12c8069ca103502cf7d3a75210b88
yeeun153 <112635570+yeeun153@users.noreply.github.com>
2022-11-07 18:46:47 +0900

    Add files via upload

---
+++ Switch4Demo.java
@@ -0,0 +1,18 @@
+package sec02;
+
+public class Switch4Demo {
+	public static void main(String[] args) {
+		int number = 3;
+
+		switch (number) {
+		case 4:
+			System.out.print("*");
+		case 3:
+			System.out.print("*");
+		case 2:
+			System.out.print("*");
+		case 1:
+			System.out.print("*");
+		}
+	}
+}
