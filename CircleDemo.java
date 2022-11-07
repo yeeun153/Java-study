b5d68ecbc86be9756966effea68354774d837867
yeeun153 <112635570+yeeun153@users.noreply.github.com>
2022-11-07 18:55:53 +0900

    Add files via upload

--- CircleDemo.java
+++ CircleDemo.java
@@ -1,17 +1,25 @@
-package sec04;
+package sec05;
 
 public class CircleDemo {
 	public static void main(String[] args) {
 		Circle myCircle = new Circle();
 
-		myCircle.radius = 10.0;
+		myCircle.setRadius(10.0);
 
-		myCircle.show(myCircle.radius, myCircle.findArea());
+		myCircle.show(myCircle.getRadius(), myCircle.findArea());
 	}
 }
 
 class Circle {
-	double radius;
+	private double radius;
+
+	public double getRadius() {
+		return radius;
+	}
+
+	public void setRadius(double r) {
+		this.radius = r;
+	}
 
 	double findArea() {
 		return 3.14 * radius * radius;
